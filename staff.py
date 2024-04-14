7d8bd5e805b2218adfac08c3ddbb838dee830173
Pratik Agrawal <105971330+Pra-Ag@users.noreply.github.com>
2024-04-15 00:00:08 +0530

    Update staff.py

--- staff.py
+++ staff.py
@@ -11,18 +11,52 @@ import string
 
 staff = Blueprint('housekeeping_staff_dashboard', __name__)
 
-@staff.route('/housekeeping_staff_dashboard/close/<int:request_id>', methods=['GET','POST'])
+#----------------- 
+
+@staff.route('/housekeeping_staff_dashboard/close_other_request/<int:request_id>', methods=['GET','POST'])
 @login_required
-def close(request_id):
+def close_other_request(request_id):
     maintenance_request_close = maintenance_request.query.get_or_404(request_id)
     maintenance_request_close.status = 'closed'
     db.session.commit()
     flash('Maintenance request closed successfully!', 'success')
-    return redirect(url_for('housekeeping_staff_dashboard.view'))
+    return redirect(url_for('housekeeping_staff_dashboard.view_other_request'))
 
-@staff.route('/housekeeping_staff_dashboard/view', methods=['GET', 'POST'])
+@staff.route('/housekeeping_staff_dashboard/view_other_request', methods=['GET', 'POST'])
 @login_required
-def view():
+def view_other_request():
     staff_id = current_user.get_id();
     requests = maintenance_request.query.filter_by(housekeeping_staff_id = staff_id, status="open").all()
-    return render_template('housekeeping_maintenance_request_closed.html', assigned = requests)
+    # select request from reqeusts whose request_id not in requires_maintenance table
+    other_requests = []
+    for request in requests:
+        if not RequiresMaintenance.query.filter_by(request_id = request.request_id).first():
+            other_requests.append(request)
+
+    return render_template('housekeeping_maintenance_other_request_closed.html', assigned = other_requests)
+
+@staff.route('/housekeeping_staff_dashboard/close_room_request/<int:request_id>', methods=['GET','POST'])
+@login_required
+def close_room_cleaning_request(request_id):
+    maintenance_request_close = maintenance_request.query.get_or_404(request_id)
+    maintenance_request_close.status = 'closed'
+    #delete the corresponding entry from requires_maintenance table
+    requires_maintenance = RequiresMaintenance.query.filter_by(request_id = request_id).first()
+    db.session.delete(requires_maintenance)
+    db.session.commit()
+    flash('Maintenance request closed successfully!', 'success')
+    return redirect(url_for('housekeeping_staff_dashboard.view_room_cleaning_request'))
+
+@staff.route('/housekeeping_staff_dashboard/view_room_request', methods=['GET', 'POST'])
+@login_required
+def view_room_cleaning_request():
+    staff_id = current_user.get_id();
+    requests = maintenance_request.query.filter_by(housekeeping_staff_id = staff_id, status="open").all()
+    # select request from reqeusts whose request_id in requires_maintenance table
+    room_cleaning_requests = []
+    for request in requests:
+        if RequiresMaintenance.query.filter_by(request_id = request.request_id).first():
+            room_cleaning_requests.append(request)
+    return render_template('housekeeping_maintenance_room_request_closed.html', assigned = room_cleaning_requests)
+
+#-----------------
